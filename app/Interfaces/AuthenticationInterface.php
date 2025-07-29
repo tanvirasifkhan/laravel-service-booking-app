89bd420981d737a00a584be9dbb24986e9dad54b
Asif Khan <asifkhan.github@gmail.com>
2025-07-29 18:12:59 +0600

    return type changed

--- app/Interfaces/AuthenticationInterface.php
+++ app/Interfaces/AuthenticationInterface.php
@@ -2,7 +2,9 @@
 
 namespace App\Interfaces;
 
+use Illuminate\Database\Eloquent\Model;
+
 interface AuthenticationInterface
 {
-    public function authenticate(array $credentials): bool;
+    public function authenticate(array $credentials): Model;
 }
