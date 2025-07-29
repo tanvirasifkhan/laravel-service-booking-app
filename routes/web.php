a45ed4438c051d756462f3446a2919f7b95174c2
Asif Khan <asifkhan.github@gmail.com>
2025-07-29 22:57:05 +0600

    json response added

--- routes/web.php
+++ routes/web.php
@@ -3,5 +3,10 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    return response()->json([
+        'message' => 'API server for Service Booking System',
+        'statusCode' => 200,
+        'Language'  => 'PHP ' . phpversion(),
+        'Framework' => 'Laravel ' . app()->version()
+    ]);
 });
