7d9d9e231afcad121096cfcd3b6b6bf1c619eb2f
Asif Khan <asifkhan.github@gmail.com>
2025-07-29 10:19:10 +0600

    routes, model, migration created for customers

---
+++ app/Models/Customer.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+use Laravel\Sanctum\HasApiTokens;
+use Illuminate\Foundation\Auth\User as Authenticatable;
+
+class Customer extends Authenticatable
+{
+    use HasApiTokens, HasFactory, SoftDeletes;
+
+    protected $table = "customers";
+
+    protected $fillable = [
+        'name',
+        'email',
+        'phone',
+        'password',
+        'address',
+    ];
+
+    protected $hidden = [
+        'password',
+        'remember_token',
+    ];
+}
--- bootstrap/app.php
+++ bootstrap/app.php
@@ -14,6 +14,9 @@ return Application::configure(basePath: dirname(__DIR__))
             Route::prefix('api/admin')
                 ->middleware('api')
                 ->group(base_path('routes/admin.php'));
+            Route::prefix('api/customer')
+                ->middleware('api')
+                ->group(base_path('routes/customer.php'));
         }
     )
     ->withMiddleware(function (Middleware $middleware): void {
