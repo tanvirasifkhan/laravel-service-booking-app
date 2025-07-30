5da06b3e9ff546982bd9af875ba795bb0f9281a9
Asif Khan <asifkhan.github@gmail.com>
2025-07-30 09:23:51 +0600

    customer feature test cases added

---
+++ tests/Feature/CustomerTest.php
@@ -0,0 +1,129 @@
+<?php
+
+namespace Tests\Feature;
+
+use App\Models\Customer;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Laravel\Sanctum\Sanctum;
+use Tests\TestCase;
+
+class CustomerTest extends TestCase
+{
+    use RefreshDatabase;
+
+    private function createCustomer(): Customer
+    {
+        return Customer::factory()->create([
+            'name' => 'John Doe',
+            'email' => 'john.doe@gmail.com',
+            'phone' => '1234567890',
+            'password' => bcrypt('john')
+        ]);
+    }
+
+    public function test_customer_registration_validation_error(): void
+    {
+        $this->postJson('api/customers/register', [])
+            ->assertUnprocessable()
+            ->assertJsonValidationErrors(['name', 'email', 'phone', 'password']);
+    }
+
+    public function test_customer_registration_unique_email_validation_error(): void
+    {
+        $this->createCustomer();
+
+        $customerData = [
+            'name' => 'New Customer',
+            'email' => 'john.doe@gmail.com',
+            'phone' => '36544689565',
+            'password' => 'password'
+        ];
+
+        $this->postJson('api/customers/register', $customerData)
+            ->assertUnprocessable()
+            ->assertJsonValidationErrors(['email']);
+    }
+
+    public function test_customer_registration_unique_phone_validation_error(): void
+    {
+        $this->createCustomer();
+
+        $customerData = [
+            'name' => 'New Customer',
+            'email' => 'john.doe2@gmail.com',
+            'phone' => '1234567890',
+            'password' => 'password'
+        ];
+
+        $this->postJson('api/customers/register', $customerData)
+            ->assertUnprocessable()
+            ->assertJsonValidationErrors(['phone']);
+    }
+
+    public function test_customer_successfull_registration(): void
+    {
+        $customerData = [
+            'name' => 'John Doe',
+            'email' => 'john.doe@gmail.com',
+            'phone' => '12345678910',
+            'password' => 'john'
+        ];
+
+        $this->postJson('api/customers/register', $customerData)
+            ->assertCreated()
+            ->assertJsonStructure(['successMessage', 'data', 'statusCode']);
+    }
+
+    public function test_customer_validation_error_while_login(): void
+    {
+        $this->postJson('api/customers/login', ['email' => '', 'password' => ''])
+            ->assertUnprocessable()
+            ->assertJsonValidationErrors(['email', 'password']);
+    }
+
+    public function test_customer_wrong_email_while_login(): void
+    {
+        $this->createCustomer();
+
+        $this->postJson('api/customers/login', ['email' => 'john.doe2@gmail.com', 'password' => 'password'])
+            ->assertUnprocessable()
+            ->assertJsonValidationErrors(['email']);
+    }
+
+    public function test_customer_wrong_credentials_while_login(): void
+    {
+        $this->createCustomer();
+
+        $this->postJson('api/customers/login', ['email' => 'john.doe@gmail.com', 'password' => 'password123'])
+            ->assertUnprocessable()
+            ->assertJsonStructure(['errorMessage', 'data', 'statusCode']);
+    }
+
+    public function test_customer_successfull_login(): void
+    {
+        $this->createCustomer();
+
+        $this->postJson('api/customers/login', ['email' => 'john.doe@gmail.com', 'password' => 'john'])
+            ->assertOk()
+            ->assertJsonStructure(['successMessage', 'data', 'statusCode']);
+    }
+
+    public function test_customer_successfull_loggedout(): void
+    {
+        Sanctum::actingAs($this->createCustomer(), ['*']);
+
+        $this->postJson('api/customers/logout')
+            ->assertOk()
+            ->assertJsonStructure(['successMessage', 'data', 'statusCode']);
+    }
+
+    public function test_read_all_customer_booking_list(): void
+    {
+        Sanctum::actingAs($this->createCustomer(), ['*']);
+
+        $this->getJson('api/customers/bookings')
+            ->assertOk()
+            ->assertJsonStructure(['successMessage', 'data', 'statusCode']);
+    }
+}
