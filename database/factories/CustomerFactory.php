913041f79b2b73d92ab0efb43cd83b89e326ee65
Asif Khan <asifkhan.github@gmail.com>
2025-07-30 09:02:16 +0600

    customer factory bug fixed

--- database/factories/CustomerFactory.php
+++ database/factories/CustomerFactory.php
@@ -3,6 +3,7 @@
 namespace Database\Factories;
 
 use Illuminate\Database\Eloquent\Factories\Factory;
+use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Str;
 
 /**
@@ -25,7 +26,6 @@ class CustomerFactory extends Factory
             'email' => fake()->unique()->safeEmail(),
             'phone' => fake()->phoneNumber(),
             'address' => fake()->address(),
-            'email_verified_at' => now(),
             'password' => static::$password ??= Hash::make('password'),
             'remember_token' => Str::random(10),
         ];
